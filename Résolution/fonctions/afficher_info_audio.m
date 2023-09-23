01b5541530f477c1fe5d12a09edd6c4cc32b764c
pires lufungula <pires00243@gmail.com>
2023-09-23 03:16:01 -0400

    Q1 afficher_info_audio(info_audio,Fs)

---
+++ Résolution/fonctions/afficher_info_audio.m
@@ -0,0 +1,20 @@
+function afficher_info_audio(info_audio,Fs)    
+  
+    % Affichage de la fréquence d'échantillonnage 
+    disp(['La fréquence d''échantillonnage (Fe) est : ', num2str(Fs), ' Hz']);
+
+    % Calcul du spectre de fréquence
+    n =length(info_audio); 
+    Fs=round(Fs);
+    
+    frequencies = (0:n-1) * (Fs / n);
+    spectrum = abs(fft(info_audio));
+    half_spectrum = spectrum(1:n/2);
+
+    % Affichage du spectre de fréquence
+    subplot(2, 1, 2);
+    plot(frequencies(1:n/2), half_spectrum);
+    title('Spectre de Fréquence');
+    xlabel('Fréquence (Hz)');
+    ylabel('Amplitude');
+end
