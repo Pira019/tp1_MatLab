ea52e1ad3589d282c805e3938e71b66e69718a1e
pires lufungula <pires00243@gmail.com>
2023-09-27 00:36:20 -0400

    R9

--- Résolution/fonctions/afficher_info_audio.m
+++ Résolution/fonctions/afficher_info_audio.m
@@ -1,21 +1,19 @@
 %cette fonction affiche des informations et représente graphiquement des données pour un fichier audio donné
-function afficher_info_audio(info_audio,Fs)    
-  
-    % Affichage de la fréquence d'échantillonnage 
+function afficher_info_audio(info_audio, Fs)
+    % Convertir info_audio en double
+    info_audio = double(info_audio);
+    
+    % Affichage de la fréquence d'échantillonnage
     disp(['La fréquence d''échantillonnage (Fe) est : ', num2str(Fs), ' Hz']);
 
     % Calcul du spectre de fréquence
-    n =length(info_audio); 
-    Fs=round(Fs);
-    
-    frequencies = (0:n-1) * (Fs / n);
-    spectrum = abs(fft(info_audio));
-    half_spectrum = spectrum(1:n/2);
+    n = round(length(double(info_audio)));
+    Fs = round(Fs);  
 
-    % Affichage du spectre de fréquence
-    subplot(2, 1, 2);
-    plot(frequencies(1:n/2), half_spectrum);
-    title('Spectre de Fréquence');
-    xlabel('Fréquence (Hz)');
-    ylabel('Amplitude');
+    % Affichage du signal temporel
+    subplot(2, 1, 1);
+    plot((0:n-1) / Fs, info_audio);
+    title('Signal Audio');
+    xlabel('Temps (s)');
+    ylabel('Amplitude'); 
 end
